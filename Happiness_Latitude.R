7d71e083bc5fbf9c3b02aa9e1b8decfa243c2190
TobyJChappell <tchappell@chapman.edu>
2021-03-10 21:17:39 -0800

    Added basic GDP vs Score plots

--- Happiness_Latitude.R
+++ Happiness_Latitude.R
@@ -4,8 +4,9 @@ library(mapproj)
 library(dplyr)
 library(tidyr)
 
+#setwd("/Users/tobychappell/Documents/CS_Courses/CS_710/Workshop1Group3")
+world_happiness <- read.csv(file = 'data/2019.csv')
 
-world_happiness <- read.csv(file = 'archive/2019.csv')
 world_happiness$Country.or.region[world_happiness$Country.or.region=="United States"] <- "USA"
 world_happiness$Country.or.region[world_happiness$Country.or.region=="United Kingdom"] <- "UK"
 countries<-map_data("world")
@@ -15,20 +16,24 @@ happiness_map<-arrange(happiness_map,group,order)
 #MAP
 ggplot(happiness_map,aes(x=long,y=lat,group=group,fill=Score)) + 
   coord_quickmap() +
-  geom_polygon()
+  geom_polygon() + 
+  scale_fill_viridis_c()
 
 #POINT
 latitude<-happiness_map %>% group_by(region) %>% summarise(score=mean(Score),lat=mean(lat)) %>% drop_na()
 latitude$lat_cat<-factor(cut(latitude$lat,seq(-60,70,1)))
 lat_group <- latitude %>% group_by(lat_cat) %>% summarise(score=mean(score))
 ggplot(lat_group,aes(x=lat_cat,y=score,color=score)) +
-  geom_point()
+  geom_point() + 
+  scale_color_viridis_c()
 
 #BAR
 latitude$lat_cat<-factor(cut(latitude$lat,seq(-60,70,10)))
 lat_group <- latitude %>% group_by(lat_cat) %>% summarise(score=mean(score),count=n())
 ggplot(lat_group,aes(x=lat_cat,fill=score)) +
-  geom_bar()
+  geom_bar() + 
+  scale_fill_viridis_c()
 
 ggplot(lat_group,aes(x=lat_cat,y=count,fill=score)) +
-  geom_bar(stat="identity")
+  geom_bar(stat="identity") + 
+  scale_fill_viridis_c()
