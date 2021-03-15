dc0551f816741efd775b62e3baf11e746c07f601
tristntran <tristntran@gmail.com>
2021-03-14 21:19:44 -0700

    happinessxdata and edits to main

--- main.R
+++ main.R
@@ -9,7 +9,17 @@ library(mapproj)
 library(tibble)
 library(purrr)
 library(readr)
+library(ggalt)
 
 
 
-happiness <- tibble(read_csv("data/happinessdata.csv"))
+happiness <- read_csv("data/happinessdata.csv")
+mid <- mean(happiness$Year)
+ggplot(happiness,aes(x=Economy,y=HappinessScore,color=Year))+
+  geom_point() +
+  scale_color_gradient2(midpoint=mid,low="blue",mid="white",
+                        high="red",space="Lab")
+
+ggplot(happiness,aes(x=Economy,y=HappinessScore,color=Region))+
+  geom_point() +
+  geom_encircle()
